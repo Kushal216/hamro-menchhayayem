59883f3d34153ddcccba6c212c64a886bf1cdb1a
Linus <bhattaraisunil76@gmail.com>
2026-01-14 13:05:38 +0545

    add menubar contents

--- components/layout/Menubar.jsx
+++ components/layout/Menubar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+'use client';
+import Link from 'next/link';
+import React from 'react';
 
 const Menubar = () => {
   return (
-    <div>Menubar</div>
-  )
-}
+    <aside>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">गृहपृष्ठ</Link>
+          </li>
+          <li>
+            <Link href="/tourism">पर्यटकिय स्थलहरु</Link>
+          </li>
+          <li>
+            <Link href="/culture">सांस्कृितिक पहिचान</Link>
+          </li>
+          <li>
+            <Link href="/schools">विद्यालयहरु</Link>
+          </li>
+          <li>
+            <Link href="/municipality">पालिका विवरण</Link>
+          </li>
+          <li>
+            <Link href="/about">हाम्रो बारेमा</Link>
+          </li>
+          <li>
+            <Link href="/contact">सम्पर्क</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
-export default Menubar
+export default Menubar;
