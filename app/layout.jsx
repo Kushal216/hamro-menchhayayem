4e2031f5b86726d65ebc86d446563f39c60586f8
Linus <bhattaraisunil76@gmail.com>
2026-01-14 17:24:58 +0545

    add bg removed logo and added favicon to site

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: 'गृहपृष्ठ - हाम्रो मेन्छ्यायेम',
   description: 'This is the homepage of Hamro Menchhayayem Website.',
+  icons: {
+    icon: '/images/logo.png',
+  },
 };
 
 export default function RootLayout({ children }) {
