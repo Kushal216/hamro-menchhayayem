21633b3228b0e912cb06fc5dd3703b7603f5a262
Linus <bhattaraisunil76@gmail.com>
2026-01-14 15:29:15 +0545

    add homepage flex layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,23 @@
-
-import Menubar from "@/components/layout/Menubar";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
+import Menubar from '@/components/layout/Menubar';
+import Navbar from '@/components/layout/Navbar';
+import Footer from '@/components/layout/Footer';
 
 export default function HomePage() {
   return (
     <>
-      <div className='flex-row'>
-        <header className='border'>
+      <div className="flex flex-col h-screen">
+        <header>
           <Navbar />
         </header>
 
-        <div className="body flex">
-
-
-        <aside className='flex-1/5 border'>
-      <Menubar/>
-        </aside>
-        <main className='flex-4/5 border'>Hamro Menchhayayem</main>
+        <div className="body flex-1 flex">
+          <aside className="flex-1/5 ">
+            <Menubar />
+          </aside>
+          <main className="flex-4/5 ">Hamro Menchhayayem</main>
         </div>
 
-        <footer className='border'>
+        <footer>
           <Footer />
         </footer>
       </div>
