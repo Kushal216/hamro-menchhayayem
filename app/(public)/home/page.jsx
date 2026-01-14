0bee53c054f377bc7b2fdf7642deb58a26f42002
Linus <bhattaraisunil76@gmail.com>
2026-01-14 12:21:10 +0545

    add jsx extensions and add homepage conponents

---
+++ app/(public)/home/page.jsx
@@ -0,0 +1,12 @@
+import Menubar from '@/components/layout/Menubar';
+import Navbar from '@/components/layout/Menubar';
+import Footer from '@/components/layout/Footer';
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Menubar />
+      <Footer />
+    </>
+  );
+}
