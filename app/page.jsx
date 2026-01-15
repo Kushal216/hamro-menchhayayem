428aa63ca00809095411364b2824ac9983385346
Linus <bhattaraisunil76@gmail.com>
2026-01-15 12:09:06 +0545

    refactor: remove redundant layouts in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,7 @@
-import Menubar from "@/components/layout/Menubar";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
-
 export default function HomePage() {
   return (
     <>
-      <div className=" bg-[#FAFAFF] flex flex-col h-screen">
-        <header>
-          <Navbar />
-        </header>
-
-        <div className="body flex-1 flex">
-          <aside className="flex-1/5 ">
-            <Menubar />
-          </aside>
-          <main className="flex-4/5 p-4">home page</main>
-        </div>
-
-        <footer>
-          <Footer />
-        </footer>
-      </div>
+          <div>home page</div>
     </>
   );
 }
