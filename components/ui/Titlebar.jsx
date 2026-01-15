032cf597315b3f68c73f0a3913adf8655539f799
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-15 15:27:03 +0545

    feat(titlebar):titlebar component made and changed to municipality

---
+++ components/ui/Titlebar.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { FaDiamond } from "react-icons/fa6";
+
+function Titlebar({ title }) {
+  return (
+    <>
+      <div className="bg-[#010BB2] px-4 py-1 text-2xl rounded font-bold text-white flex items-center gap-2">
+        <span>{title}</span>
+        <FaDiamond size={18} className="ml-auto text-white" />
+      </div>
+    </>
+  );
+}
+
+export default Titlebar;
