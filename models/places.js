a43a7ff23bfdf66e4e7703cb3ab767d2afa267cf
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-18 13:06:23 +0545

    api created: schools get and post

--- models/places.js
+++ models/places.js
@@ -1,49 +1,48 @@
 import mongoose from 'mongoose';
 import { connectMongoDB } from '@/lib/db.js';
 
-import { connectMongoDB } from "@/lib/db.js";
 
 const placesSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    gallery: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'image',
-      },
-    ],
-    coverImage: {
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  gallery: [
+    {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'image',
     },
-    location: {
-      type: String,
-      default: 'Menchhayayem rural municipality',
-    },
-    video: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'video',
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    subCategory: {
-      type: String,
-      required: true,
-    },
-    likesCount: {
-      type: Number,
-      default: 0,
-      min: 0,
-    },
+  ],
+  coverImage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'image',
   },
+  location: {
+    type: String,
+    default: 'Menchhayayem rural municipality',
+  },
+  video: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'video',
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subCategory: {
+    type: String,
+    required: true,
+  },
+  likesCount: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+},
   { timestamps: true }
 );
 
