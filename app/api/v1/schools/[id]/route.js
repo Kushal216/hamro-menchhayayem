3d83a658ba0037e288f3c89e9a5df31d0bc680ba
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-18 16:17:55 +0545

    api/v1/school/id: all api made and tested

--- app/api/v1/schools/[id]/route.js
+++ app/api/v1/schools/[id]/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import School from '@/models/school.js';
+import culture from '@/models/culture';
 
 /**
  * @swagger
@@ -68,21 +69,114 @@ export async function GET(request, res) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
-export async function PUT(request, { params }) {
-  const { id } = params;
 
 
-  return Response.json({
-    message: 'UPDATE school',
-    id: id,
-  });
+export async function PUT(request, res) {
+  const { id } = res.params;
+  const body = await request.json();
+  try {
+    const school = await School.findById(id);
+    if (!culture) {
+      return NextResponse.json({ error: "Not Found" }, { status: 404 });
+    }
+    const dbResponse = await School.replaceOne(
+      { _id: id },
+      {
+        title: body.title,
+        description: body.title,
+        gallery: body.gallery,
+        coverImage: body.coverImage,
+        location: body.location,
+        video: body.video,
+        category: body.category,
+        phoneNo: body.phoneNo,
+      })
+    return NextResponse.json({
+      message: `replaced school ${body.title} with ${body.title}`,
+      id: id,
+      data: dbResponse
+    });
+  } catch {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
 }
 
-export async function DELETE(request, { params }) {
-  const { id } = params;
+/**
+ * @swagger
+ * /api/schools/{id}:
+ *   put:
+ *     summary: Update item with :id
+ *     tags:
+ *       - school
+ *     description: Returns a list of school
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                   name:
+ *                     type: string
+ */
+export async function PATCH(req, res) {
+  const { id } = await res.params;
+  const body = await req.json();
 
-  return Response.json({
-    message: 'DELETE school',
-    id: id,
-  });
+  try {
+    const updated = await School.findByIdAndUpdate(
+      id,
+      { $set: body }, // only update provided fields
+      { new: true, runValidators: true }
+    );
+
+    if (!updated) {
+      return NextResponse.json({ error: 'Not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: `Updated school ${updated.title}`,
+      data: updated,
+    });
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(req, res) {
+  //validate user
+
+  const { id } = await res.params;
+
+  try {
+    const school = await School.findByIdAndDelete(id);
+
+    if (school) {
+      return Response.json({
+        message: `deleted ${school.title}`,
+        data: school,
+      });
+    } else {
+      return Response.json(
+        {
+          message: "The school with the given id doesn't exist.",
+          id: id,
+        },
+        { status: 404 }
+      );
+    }
+  } catch (err) {
+    return Response.json(
+      {
+        message: 'Some error occurred in the database',
+        error: err,
+      },
+      { status: 500 }
+    );
+  }
 }
