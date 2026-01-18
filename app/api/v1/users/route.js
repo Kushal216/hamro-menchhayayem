2559ea66b27c2a730352c17348ceec8692625174
Linus <bhattaraisunil76@gmail.com>
2026-01-18 21:59:26 +0545

    fix: users route.js

--- app/api/v1/users/route.js
+++ app/api/v1/users/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import User from '@/models/user';
 import bcrypt from 'bcryptjs';
+
 /**
  * @swagger
  * /api/v1/users:
@@ -26,25 +27,56 @@ import bcrypt from 'bcryptjs';
  */
 export async function GET(req) {
   const users = await User.find({});
-
   return NextResponse.json({ message: 'users fetched', data: users });
 }
 
+/**
+ * @swagger
+ * /api/v1/users:
+ *   post:
+ *     summary: add user to the database
+ *     tags:
+ *       - places
+ *     description: adds user in body to the db
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                   name:
+ *                     type: string
+ */
 export async function POST(req) {
-  const { email, password } = await req.json();
+  const { name, email, password } = await req.json();
   const saltValue = 10;
   try {
     const hashedPassword = await bcrypt.hash(password, saltValue);
-    await User.create({
+    const user = await User.create({
+      name,
       email,
       password: hashedPassword,
-    })
-    return NextResponse.jsonm({
-      message: "created User"
-    }, {
-      status: 201
-    })
-  } catch {
-    return NextResponse.json({ ERROR: "failed to create the user" }, { status: 401 })
+    });
+
+    return NextResponse.json(
+      {
+        message: `created User ${user.main}.`,
+        user:user,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { ERROR: 'failed to create the user', details: err },
+      { status: 500 }
+    );
   }
 }
