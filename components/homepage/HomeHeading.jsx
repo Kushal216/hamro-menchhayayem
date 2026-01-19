955e6da96a6712a0fb08dfc5afe71a67773db0d5
Linus <bhattaraisunil76@gmail.com>
2026-01-19 11:35:13 +0545

    feat: add details route

--- components/homepage/HomeHeading.jsx
+++ components/homepage/HomeHeading.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const HomeHeading = ({title, color}) => {
+const HomeHeading = ({ title, color, fixed }) => {
   return (
-    <div className="font-bold text-2xl box-border border-l-8 pl-2 border-[#FF3B00] m-2 my-4">
+    <div
+      className={
+        `font-bold text-${color}  box-border border-l-8 pl-2 border-[#FF3B00] m-2 my-4` +
+        (fixed === true
+          ? ' bg-[#00000090] backdrop-blur-[1px] rounded-r mt-2 p-1 pr-2 ml-0 text-md md:rounded-r-lg md:text-xl'
+          : ' text-lg md:text-2xl')
+      }
+    >
       {title}
     </div>
   );
-}
+};
 
-export default HomeHeading
+export default HomeHeading;
