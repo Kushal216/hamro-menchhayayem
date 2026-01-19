0c85bd1d9237dfa2b48adff80e5621bd585a39a2
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-19 12:42:02 +0545

    feat(add): admin dash/people added

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -1,14 +1,17 @@
 import Place from "@/components/Admin/Place";
 import Culture from "@/components/Admin/Culture";
 import School from "@/components/Admin/School";
+import People from "@/components/Admin/People";
 
 export default function AdminPage() {
   return (
     <div>
-      <h1>Admin Dashboard</h1>
+          <h1>Admin Dashboard</h1>
+          <People/>
       <Place />
       <Culture />
-      <School />
+          <School />
+          
     </div>
   );
 }
