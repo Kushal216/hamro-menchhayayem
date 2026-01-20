cdc7a04e8a0df65058150d0449af21a734260b60
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-20 13:22:48 +0545

    added litererature in homepage

--- components/homepage/ImportantPlacesSection.jsx
+++ components/homepage/ImportantPlacesSection.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import SectionCard from './SectionCard';
+import React from "react";
+import SectionCard from "./SectionCard";
 
 const places = [
   {
-    title: 'हाम्रा संस्कृती',
-    subtitle: 'हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस',
-    imageUrl: '/images/image.png',
-    route: '/cultures',
-    iconUrl: '/section-icons/cultures.png',
+    title: "हाम्रा संस्कृती",
+    subtitle: "हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस",
+    imageUrl: "/images/image.png",
+    route: "/cultures",
+    iconUrl: "/section-icons/cultures.png",
   },
   {
-    title: 'हाम्रा ठाउँहरु',
-    subtitle: 'हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस',
-    imageUrl: '/images/image.png',
-    iconUrl: '/section-icons/places.png',
-    route: '/places',
+    title: "हाम्रा ठाउँहरु",
+    subtitle: "हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस ।",
+    imageUrl: "/images/image.png",
+    iconUrl: "/section-icons/places.png",
+    route: "/places",
   },
   {
-    title: 'हाम्रा विद्यालयहरु',
-    subtitle: 'हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस',
-    imageUrl: '/images/image.png',
-    route: '/schools',
-    iconUrl: '/section-icons/schools.png',
+    title: "हाम्रा विद्यालयहरु",
+    subtitle: "हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस ।",
+    imageUrl: "/images/image.png",
+    route: "/schools",
+    iconUrl: "/section-icons/schools.png",
+  },
+  {
+    title: "लेखहरु",
+    subtitle: "हाम्रो मेन्छ्यायेमका लेखकहरुले लेख्नु भएका लेखहरु पढ्नुहोस् ।",
+    imageUrl: "/images/image.png",
+    route: "/literature",
+    iconUrl: "/section-icons/schools.png",
   },
 ];
 
