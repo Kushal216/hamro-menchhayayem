b05101a9558a986825874ce2da754aa816a11885
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-20 23:51:50 +0545

    add: admin/place and admin/school added

--- app/(admin)/admin/cultures/page.jsx
+++ app/(admin)/admin/cultures/page.jsx
@@ -1,19 +1,13 @@
-import CultureForm from '@/components/Admin/AddItems/AddCulture';
-import ManageData from '@/components/Admin/ManageData';
-import ToggleForm from '@/components/Admin/ToggleForm';
-import fetchData from '@/lib/fetchItem';
-import React from 'react';
+import ManageData from "@/components/Admin/ManageData";
+import ToggleForm from "@/components/Admin/ToggleForm";
+import React from "react";
 
 const page = () => {
-  const res = fetchData('cultures');
-
   return (
     <>
-    <div className='flex justify-center'>
-    </div>
+      <div className="flex justify-center"></div>
       <div>
-        {/* <div className='text-xl font-bold'>Manage Data</div> */}
-      <ToggleForm title="Add a culture"/>
+        <ToggleForm title="Add a Culture" />
         <ManageData route="cultures" />
       </div>
     </>
