e0e2adda0c026fa52fa6a6ac817c098952e5ed45
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-01-21 19:10:52 +0545

    add: audio and menchhayayem gan to municiplity page

--- components/homepage/SectionCard.jsx
+++ components/homepage/SectionCard.jsx
@@ -1,14 +1,31 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const SectionCard = ({ item }) => {
-  const scale = item.route == '/places';
+  const scale = item.route == "/places";
 
   return (
     <Link
-      href={item.route ? item.route : '/'}
-      className="relative w-[90%] aspect-10/3 md:h-full xl:h-78 col-span-3 md:col-span-1  md:aspect-4/3 inline-block md:w-full  rounded-2xl overflow-clip shadow-lg hover:shadow-[#00000060] hover:scale-101 hover:-translate-y-0.5 select-none cursor-pointer mx-auto md:mb-8"
+      href={item.route ? item.route : "/"}
+      className="
+  relative 
+  w-[90%] md:w-full 
+  aspect-10/3 md:aspect-4/3 
+  md:h-full xl:h-78 
+  col-span-3 md:col-span-1 
+  inline-block 
+  rounded-2xl 
+  overflow-clip 
+  shadow-lg 
+  hover:shadow-[#00000060] 
+  hover:scale-[1.01] 
+  hover:-translate-y-0.5 
+  select-none 
+  cursor-pointer 
+  mx-auto 
+  md:mb-8
+"
     >
       <div className="absolute h-full w-full bg-linear-to-t from-[#00000099] to-[#00000040] backdrop-blur-[2px] z-2">
         <div className="flex md:flex-col h-full gap-4 md:gap-1 items-center justify-center px-2">
@@ -17,7 +34,7 @@ const SectionCard = ({ item }) => {
               src={item.iconUrl}
               alt={`${item.title} icon`}
               fill
-              className={`object-contain ${scale && 'scale-144 group-hover:scale-150 group-hover:-translate-y-0.5'}`}
+              className={`object-contain ${scale && "scale-144 group-hover:scale-150 group-hover:-translate-y-0.5"}`}
             />
           </div>
           <div className="md:text-center flex-1 md:flex-none">
