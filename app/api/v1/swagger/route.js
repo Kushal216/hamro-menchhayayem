6058ed2b5446b32a962cd0ab09d6b2e3ee911f87
Linus <bhattaraisunil76@gmail.com>
2026-01-22 14:01:16 +0545

    fix: swagger runtime issue

--- app/api/v1/swagger/route.js
+++ app/api/v1/swagger/route.js
@@ -1,15 +1,18 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 import { NextResponse } from 'next/server';
+import path from 'path';
+
+export const runtime = 'nodejs';
 
 const swaggerSpec = swaggerJSDoc({
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Hamro-menchhayayem API ',
+      title: 'Hamro-menchhayayem API',
       version: '1.0.0',
     },
   },
-  apis: ['./app/api/v1/**/route.js'],
+  apis: [path.join(process.cwd(), 'app/api/v1/**/route.js')],
 });
 
 export async function GET() {
--- components/homepage/HomeHeading.jsx
+++ components/homepage/HomeHeading.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { IoArrowBackSharp } from 'react-icons/io5';
 
-const HomeHeading = ({ title, color, fixed, back }) => {
+const HomeHeading = ({ title, fixed, back }) => {
   return (
     <div
       className={
-        `font-bold text-${color}  box-border   m-2 my-4 flex items-center` +
+        `font-bold text-black  box-border   m-2 my-4 flex items-center` +
         (fixed === true
           ? ' bg-[#00000090] backdrop-blur-[1px] rounded-r mt-2 p-1 pr-2 ml-0 text-md md:rounded-r-lg md:text-xl'
           : ' text-lg md:text-2xl') +
