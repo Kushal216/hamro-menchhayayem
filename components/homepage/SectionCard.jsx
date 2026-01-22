ae30ff74ea89a81684d759a028e305383fd1c908
Linus <bhattaraisunil76@gmail.com>
2026-01-22 17:39:37 +0545

    add section-bg images

--- components/homepage/SectionCard.jsx
+++ components/homepage/SectionCard.jsx
@@ -1,40 +1,40 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const SectionCard = ({ item }) => {
-  const scale = item.route == "/places";
+  const scale = item.route == '/places';
 
   return (
     <Link
-      href={item.route ? item.route : "/"}
+      href={item.route ? item.route : '/'}
       className="
-  relative 
-  w-[90%] md:w-full 
-  aspect-10/3 md:aspect-4/3 
-  md:h-full xl:h-78 
-  col-span-3 md:col-span-1 
-  inline-block 
-  rounded-2xl 
-  overflow-clip 
-  shadow-lg 
-  hover:shadow-[#00000060] 
-  hover:scale-[1.01] 
-  hover:-translate-y-0.5 
-  select-none 
-  cursor-pointer 
-  mx-auto 
+  relative
+  w-[90%] md:w-full
+  aspect-10/3 md:aspect-4/3
+  md:h-full xl:h-78
+  col-span-3 md:col-span-1
+  inline-block
+  rounded-2xl
+  overflow-clip
+  shadow-lg
+  hover:shadow-[#00000060]
+  hover:scale-[1.01]
+  hover:-translate-y-0.5
+  select-none
+  cursor-pointer
+  mx-auto
   md:mb-8
 "
     >
-      <div className="absolute h-full w-full bg-linear-to-t from-[#00000099] to-[#00000040] backdrop-blur-[2px] z-2">
+      <div className="absolute h-full w-full bg-linear-to-t from-black to-black/40 backdrop-blur-[2px] z-2">
         <div className="flex md:flex-col h-full gap-4 md:gap-1 items-center justify-center px-2">
           <div className="w-10 md:w-12 lg:w-17 xl:w-20 aspect-square relative mb-2 xl:mb-8 ml-2 md:ml-0">
             <Image
               src={item.iconUrl}
               alt={`${item.title} icon`}
               fill
-              className={`object-contain ${scale && "scale-144 group-hover:scale-150 group-hover:-translate-y-0.5"}`}
+              className={`object-contain ${scale && 'scale-144 group-hover:scale-150 group-hover:-translate-y-0.5'}`}
             />
           </div>
           <div className="md:text-center flex-1 md:flex-none">
