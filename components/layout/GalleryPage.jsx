d9dea3986792008134f1425cb28578078346b856
Linus <bhattaraisunil76@gmail.com>
2026-01-22 19:30:24 +0545

    fix gallery page : added links and some hover effects

--- components/layout/GalleryPage.jsx
+++ components/layout/GalleryPage.jsx
@@ -1,8 +1,9 @@
-import HomeHeading from "@/components/homepage/HomeHeading";
-import Image from "next/image";
-import React from "react";
-import BackButton from "./BackButton";
-import { fetchItem } from "@/lib/fetchItem";
+import HomeHeading from '@/components/homepage/HomeHeading';
+import Image from 'next/image';
+import React from 'react';
+import BackButton from './BackButton';
+import { fetchItem } from '@/lib/fetchItem';
+import Link from 'next/link';
 
 let i = 0;
 const GalleryPage = async ({ route, id }) => {
@@ -16,25 +17,37 @@ const GalleryPage = async ({ route, id }) => {
       <BackButton gallery />
 
       <div className="flex flex-wrap m-2 gap-2">
-        <div key={i} className="aspect-square w-40 relative col-span-1">
+        <Link
+          href={cover}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={i}
+          className="hover:shadow-lg hover:scale-105 hover:-translate-y-1 aspect-square w-40 relative col-span-1"
+        >
           <Image
             src={cover}
             fill
             className="object-cover rounded"
             alt="image"
           />
-        </div>
+        </Link>
 
         {gallery.map((url) => (
-          <div key={i} className="aspect-square w-40 relative">
+          <Link
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={i}
+            className="hover:shadow-lg hover:scale-105 hover:-translate-y-1 aspect-square w-40 relative"
+          >
             <Image
               key={i++}
-              src={url}
+              src={url || '/images/fallback-image.jpg'}
               fill
               className="object-cover rounded"
               alt="image"
             />
-          </div>
+          </Link>
         ))}
       </div>
     </>
