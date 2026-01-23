bef08dfe8c03238589bd5d45b281a6f05ae4c3a9
Linus <bhattaraisunil76@gmail.com>
2026-01-23 22:58:41 +0545

    feat: added default og image and changed fallback image

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,37 @@
-import HeroImage from "@/components/homepage/HeroImage";
-import HomeHeading from "@/components/homepage/HomeHeading";
-import ImportantPlacesSection from "@/components/homepage/ImportantPlacesSection";
-import ToastTrigger from "@/components/ToastTrigger";
+import HeroImage from '@/components/homepage/HeroImage';
+import HomeHeading from '@/components/homepage/HomeHeading';
+import ImportantPlacesSection from '@/components/homepage/ImportantPlacesSection';
+import ToastTrigger from '@/components/ToastTrigger';
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-full pt-1">
       <HeroImage />
-      <HomeHeading title={"हाम्रो विशेषता"} color={"red"} />
+      <HomeHeading title={'हाम्रो विशेषता'} color={'red'} />
       <ImportantPlacesSection />
       <ToastTrigger
-        message={"Welcome to Hamro Menchhayayem"}
-        type={"success"}
+        message={'Welcome to Hamro Menchhayayem'}
+        type={'success'}
       />
     </div>
   );
+}
+
+export async function generateMetadata({ params }) {
+  return {
+    title: 'हाम्रो मेन्छयायेम',
+    openGraph: {
+      title: 'हाम्रो मेन्छयायेम',
+      type: 'article',
+      url: `https://hamromenchhayayem.vercel.app`,
+      images: [
+        {
+          url: 'https://hamromenchhayayem.vercel.app/images/og-image.webp',
+          width: 1200,
+          height: 630,
+          alt: 'हाम्रो मेन्छयायेम',
+        },
+      ],
+    },
+  };
 }
