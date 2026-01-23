2a257140b85925a7d230eab1ac826cb4baf073e3
Linus <bhattaraisunil76@gmail.com>
2026-01-23 12:06:28 +0545

    fix loader issue

---
+++ app/(admin)/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-[60vh] flex items-center justify-center">
+      <div className="h-12 w-12 rounded-full border-4 border-gray-300 border-t-[#018378] animate-spin" />
+    </div>
+  );
+}
