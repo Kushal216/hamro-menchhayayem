f118f1612c9b389ce5ff80a9f4d2581850d9954f
Linus <bhattaraisunil76@gmail.com>
2026-08-20 13:44:27 +0545

    fix: use user.name instead of user.main in create user response

--- app/api/v1/users/route.js
+++ app/api/v1/users/route.js
@@ -47,7 +47,7 @@ export async function POST(req) {
 
     return NextResponse.json(
       {
-        message: `created User ${user.main}.`,
+        message: `created User ${user.name}.`,
         user: user,
       },
       {
