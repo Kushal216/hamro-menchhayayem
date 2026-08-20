171b21d3089b49a8ce6e9e67c1c1f466815c289f
Linus <bhattaraisunil76@gmail.com>
2026-08-20 18:31:21 +0545

    fix: remove red left border from RouteTitle heading

--- components/layout/RouteTitle.jsx
+++ components/layout/RouteTitle.jsx
@@ -4,7 +4,7 @@ import { IoArrowBackSharp } from 'react-icons/io5';
 
 const RouteTitle = ({ title }) => {
   return (
-    <div className="flex items-center gap-3 px-4 py-3 mt-4 mx-2 border-l-4 border-[#FF3B00] bg-gray-50 rounded-r-lg">
+    <div className="flex items-center gap-3 px-4 py-3 mt-4 mx-2 bg-gray-50 rounded-r-lg">
       <Link
         href="/"
         className="flex items-center justify-center w-9 h-9 rounded-full bg-white shadow hover:bg-gray-100 transition-colors shrink-0"
