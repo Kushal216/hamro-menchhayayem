b4a9572625f6d0e6cec968fd0a58a710f8da4645
Linus <bhattaraisunil76@gmail.com>
2026-08-20 15:00:55 +0545

    perf: make RootLayoutBody a server component by removing unused pathname check

--- components/layout/RootLayoutBody.jsx
+++ components/layout/RootLayoutBody.jsx
@@ -1,20 +1,9 @@
-'use client';
-import React from 'react';
 import NavigationComponents from './NavigationComponents';
-import { usePathname } from 'next/navigation';
 
 const RootLayoutBody = ({ children }) => {
-  const paths = usePathname().replace(/\/$/, '').split('/');
-  const route = paths[1];
-  const menuNeeded = !(
-    route == 'admin' ||
-    route == 'login' ||
-    paths.length > 2
-  );
-
   return (
     <div className={`grid grid-cols-5 min-h-screen`}>
-      <NavigationComponents >{children}</NavigationComponents>
+      <NavigationComponents>{children}</NavigationComponents>
     </div>
   );
 };
