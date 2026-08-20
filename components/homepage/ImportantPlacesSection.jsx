252448d030933f43fddd383045798326fd17674c
Linus <bhattaraisunil76@gmail.com>
2026-08-20 18:04:06 +0545

    feat: add schools section, fix literature icon, fix duplicate subtitles, and make grid responsive

--- components/homepage/ImportantPlacesSection.jsx
+++ components/homepage/ImportantPlacesSection.jsx
@@ -4,30 +4,37 @@ import SectionCard from "./SectionCard";
 const places = [
   {
     title: 'हाम्रा संस्कृती',
-    subtitle: 'हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस ।',
+    subtitle: 'हाम्रो मेन्छ्यायेमका सांस्कृतिक पहिचान र परम्पराहरु हेर्नुहोस्।',
     imageUrl: '/section-icons/culture-bg.webp',
     route: '/cultures',
     iconUrl: '/section-icons/cultures.png',
   },
   {
     title: 'हाम्रा ठाउँहरु',
-    subtitle: 'हाम्रो मेन्छ्यायेमका रमणीय स्थानहरु भ्रमण गर्नुहोस ।',
+    subtitle: 'हाम्रो मेन्छ्यायेमका रमणीय पर्यटकिय स्थलहरु भ्रमण गर्नुहोस्।',
     imageUrl: '/images/hero-images/hero1.webp',
     iconUrl: '/section-icons/places.png',
     route: '/places',
   },
   {
+    title: 'विद्यालयहरु',
+    subtitle: 'हाम्रो मेन्छ्यायेमका विद्यालयहरुबारे जान्नुहोस्।',
+    imageUrl: '/section-icons/culture-bg.webp',
+    route: '/schools',
+    iconUrl: '/section-icons/schools.png',
+  },
+  {
     title: 'लेखहरु',
-    subtitle: 'हाम्रो मेन्छ्यायेमका लेखकहरुले लेख्नु भएका लेखहरु पढ्नुहोस ।',
+    subtitle: 'हाम्रो मेन्छ्यायेमका लेखकहरुले लेख्नु भएका लेखहरु पढ्नुहोस्।',
     imageUrl: '/section-icons/literature-bg.webp',
     route: '/literature',
-    iconUrl: '/section-icons/schools.png',
+    iconUrl: '/section-icons/cultures.png',
   },
 ];
 
 const ImportantPlacesSection = () => {
   return (
-    <div className="grid grid-cols-3 justify-between px-2 gap-3 mb-5 min-w-full w-fit">
+    <div className="grid grid-cols-2 md:grid-cols-4 justify-between px-2 gap-3 mb-5 min-w-full w-fit">
       {places.map((item, index) => (
         <SectionCard key={index} item={item} />
       ))}
