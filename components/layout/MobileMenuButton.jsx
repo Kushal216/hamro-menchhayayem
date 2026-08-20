164309240d97171195e2106dde9b96e9b3ff4025
Linus <bhattaraisunil76@gmail.com>
2026-08-20 17:59:11 +0545

    fix: replace 3-dot icon with hamburger, add ARIA attributes and 44px touch target

--- components/layout/MobileMenuButton.jsx
+++ components/layout/MobileMenuButton.jsx
@@ -1,16 +1,21 @@
 'use client';
 import { CgClose } from 'react-icons/cg';
-import { HiDotsVertical } from 'react-icons/hi';
+import { HiMenu } from 'react-icons/hi';
 
 const MobileMenuButton = ({ isOpen, toggleMenu }) => {
   return (
-    <div onClick={toggleMenu} className="lg:hidden">
+    <button
+      onClick={toggleMenu}
+      className="lg:hidden p-3 -mr-3 min-w-[44px] min-h-[44px] flex items-center justify-center"
+      aria-label={isOpen ? 'Close navigation menu' : 'Open navigation menu'}
+      aria-expanded={isOpen}
+    >
       {!isOpen ? (
-        <HiDotsVertical color="white" size={24} />
+        <HiMenu color="white" size={24} />
       ) : (
         <CgClose color="white" size={24} />
       )}
-    </div>
+    </button>
   );
 };
 
